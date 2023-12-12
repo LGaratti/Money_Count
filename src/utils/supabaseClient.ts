aa356fbd24be106ae94316b1cf3902eaa7f51e0f
Luca Garatti <l.garatti1996@gmail.com>
2023-12-12 23:58:19 +0100

    fix import

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Label, Operation, OperationsAction } from "../interfaces/Operation";
-import { fromJsonToLabels, fromJsonToOperations, initOperations } from "./OperationUtils";
+import { initOperations } from "./OperationUtils";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
