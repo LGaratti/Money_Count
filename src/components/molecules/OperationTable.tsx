5b5e9d214fb65014b1b16bf36f79e677d5d53771
Luca Garatti <l.garatti1996@gmail.com>
2023-12-12 23:47:13 +0100

    Add some information in OperationTable
    Added handling of amount and labels

--- src/components/molecules/OperationTable.tsx
+++ src/components/molecules/OperationTable.tsx
@@ -1,34 +1,54 @@
-import { Table, TableContainer, Tbody, Td, Th, Thead, Tr} from '@chakra-ui/react'
+import { Table, TableContainer, TableProps, Tbody, Td, Th, Thead, Tr} from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next';
 import i18n from '../../locales/i18n';
-import { Operation } from '../../interfaces/Operation';
+import { Label, Operation } from '../../interfaces/Operation';
 
-interface Props {
-  operations: Operation[];
+// interface Props {
+//   operations: Operation[];
+// }
+interface OperationCardProps extends TableProps {
+  operations?: Operation[];
 }
 
-const OperationTable = ({operations}: Props) => {
-  
+// const OperationTable = ({operations}: Props) => {
+  export const OperationTable = ({operations,...props}: OperationCardProps) => {
   const {t} = useTranslation('ns1',{ i18n } );
 
+  const amountColor = (amount: number) => {
+    if (amount > 0) return 'green.500';
+    if (amount < 0) return 'red.500';
+  };
+
+  const printWithSign = (amount:number) => {
+    if (amount > 0 ) return "+"+amount;
+    else if (amount < 0 ) return "-"+amount;
+    return amount;
+  }
+
+  const printLabels = (labels:Label[]) => {
+    let stringa:string= ""; 
+    labels.map((label) => (
+      stringa += label.name+" "
+    ))
+    return stringa;
+  }
   return (
     <>
       <TableContainer>
-      <Table variant='simple'>
+      <Table variant='simple'{...props}>
         <Thead>
           <Tr>
-            <Th>{t('id')}</Th>
             <Th>{t('name')}</Th>
-            <Th>{t('population')}</Th>
+            <Th>{t('amount')}</Th>
+            <Th>{t('labels')}</Th>
           </Tr>
         </Thead>
         <Tbody>
-          {operations.map((operation) => (
+          {operations?.map((operation) => (
             <Tr key = {operation.operation_id}>
-              <Td>{operation.operation_id}</Td>
-              <Td>{operation.name}</Td>
-              <Td>{operation?.description}</Td>
-              {/* <Td>{operation?.labels.map((label) => ( label.name))}</Td> */}
+              <Td>{operation?.name}</Td>
+              <Td color={amountColor(operation?.amount)}>{printWithSign(operation?.amount)}</Td>
+              <Td>{printLabels(operation?.labels)}</Td>
             </Tr>
           ))}
         </Tbody>
