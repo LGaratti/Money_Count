363bc38dac49190f5902b0fd5fdfa579cd1b69a8
Luca Garatti <l.garatti1996@gmail.com>
2023-12-12 23:46:36 +0100

    supabase API updated
    Fix labels managment

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -7,43 +7,41 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+export interface OperationLabel {
+  operation:Operation,
+  label:Label
+}
+
 export async function getOpsFromServer(operationArrayReducer: React.Dispatch<OperationsAction>) {
   const { data , error } = await supabase
-  .from('operations')
+  .from('operations_labels')
   .select(`
-    operation_id,
-    name,
-    active,
-    description,
-    amount,
-    periodical:periodicals (periodical_id, amount, unit, last_timestamp, first_timestamp)
-  `);
+    operation:operations (operation_id,name,active,description,amount,periodical:periodicals (periodical_id, amount, unit, last_timestamp, first_timestamp)),
+    label:labels (label_id, name, description, color_rgb)
+  `)
+  .returns<OperationLabel[]>();
+
   if (error)
-    console.log(error);
-  const operations: Operation[] = fromJsonToOperations(data);
-  if (operations) {
-    for (const op of operations) {
-      // Converti op.operation_id in un numero intero
-      const operationId = parseInt(op.operation_id, 10);      
-      const { data: labelsData, error: labelsError } = await supabase
-        .from('operations_labels')
-        .select(`
-            label:labels (label_id, name, description, color_rgb)
-        `)
-        .eq('operation_id', operationId);
+    throw error;
+  if(!data)
+    throw new Error("empty response from server");
+    const tempOperations: Operation[] = [];
+
+    data.forEach((operationLabel) => {
+      const foundOperation = tempOperations.find(operation => operation.operation_id === operationLabel.operation.operation_id);
   
-      if (labelsError)
-        console.log(labelsError);
-  
-      const labels: Label[] = fromJsonToLabels(labelsData);
-      if (labels) {
-          // Aggiungi le labels all'operazione
-          op.labels = labels;
+      if (!foundOperation) {
+        // Se l'operazione non esiste, creala con un array vuoto per labels
+        const newOperation = { ...operationLabel.operation, labels: [operationLabel.label] };
+        tempOperations.push(newOperation);
+      } else {
+        // Se l'operazione esiste già, aggiungi la label (assicurandosi che labels sia un array)
+        foundOperation.labels = foundOperation.labels || []; // Assicura che labels sia un array
+        foundOperation.labels.push(operationLabel.label);
       }
-    }
-  }
-  initOperations(operationArrayReducer, operations);
-  console.log("Operazioni ricevute dal server:", operations);
+    });
+  initOperations(operationArrayReducer, tempOperations);
+  console.log("Operazioni ricevute dal server:", tempOperations);
 }
 
 export async function setOpsFromServer(operations: Operation[]) {
