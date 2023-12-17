1dbaee685488574d3ac4b7cf455219fce8c1458c
Luca Garatti <l.garatti1996@gmail.com>
2023-12-17 19:29:45 +0100

    Add chackra theme configurations
    https://chakra-ui.com/docs/styled-system/customize-theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,14 @@
 import {
   extendTheme,
   theme,
+  withDefaultColorScheme,
   type ThemeConfig,
 } from "@chakra-ui/react";
 
+export const COLOR_THEME = "primary";
+
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "light",
   useSystemColorMode: false,
 };
 
@@ -13,39 +16,59 @@ export default extendTheme(
   {
     ...theme,
     config,
+    styles: {
+      global: {
+        body: {
+          background: `pageBackground1`,
+          color: "label",
+          userSelect: "none",
+          WebkitUserSelect: "none",
+          MozUserSelect: "none",
+          msUserSelect: "none",
+        },
+      },
+    },
+    colors: {
+      brand:{
+        100: "#40a664",
+        900: "#781cca"
+      },
+      primary:{
+        50: '#e5fbee',
+        100: '#c4ebd2',
+        200: '#a1ddb6',
+        300: '#7dcd96',
+        400: '#59bf76',
+        500: '#40a664',
+        600: '#308152',
+        700: '#205c3e',
+        800: '#0f3827',
+        900: '#00140a',
+      },
+      secondary:{
+        50: '#f6e6ff',
+        100: '#dcbaf8',
+        200: '#c28ef1',
+        300: '#aa61ea',
+        400: '#9234e3',
+        500: '#781cca',
+        600: '#5e159d',
+        700: '#430e71',
+        800: '#280845',
+        900: '#0f011b',
+      }
+    },
     fonts: {
       heading: `'Outfit Variable', sans-serif`,
       body: `'Outfit Variable', sans-serif`,
     },
-    colors: {
-      primary: {
-        15: "#F3F3D7",
-        25: "#DDDDBC",
-        50: "#E4E494",
-        100: "#D3D16B",
-        200: "#85BC6B",
-        300: "#4CBA6B",
-        400: "#528D47",
-        500: "#325A34",
-        600: "#22431C",
-        700: "#263927",
-        800: "#242D25",
-        900: "#2F3630",
+    semanticTokens: {
+      colors: {
+        secondary: "#781cca",
+        pageBackground1: `${COLOR_THEME}.100`,
+        label: "#00140a",
       },
-      secondary: {
-        15: "#D6D3DF",
-        25: "#BDB7D0",
-        50: "#BFB0D3",
-        100: "#9C8BCC",
-        200: "#8783BF",
-        300: "#726CBD",
-        400: "#7C50A3",
-        500: "#410E6E",
-        600: "#381A53",
-        700: "#322141",
-        800: "#2B282E",
-        900: "#332F36",
-      }
     },
   },
 );
+withDefaultColorScheme({ colorScheme: `${COLOR_THEME}` })
