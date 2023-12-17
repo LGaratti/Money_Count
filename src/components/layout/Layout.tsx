f758e3eeb19205435c1ea5bf547d84dc4faaedf2
Luca Garatti <l.garatti1996@gmail.com>
2023-12-17 21:50:06 +0100

    Start Adding Theme switch on Navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,21 @@
-import { VStack } from "@chakra-ui/react";
+import { Image, VStack, useColorMode } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { TopBar } from "./TopBar";
 
 function Layout() {
+  const { colorMode } = useColorMode();
   return (
-    <VStack w="100vw" spacing={0}>
+    <VStack w="100vw" h="100vh" spacing={0} overflow="hidden">
       <TopBar />
+      <Image
+        w="100%"
+        h="100vh" // Imposta l'altezza al 100% della viewport
+        src={colorMode === "dark" ? "background-dark.svg" : "background-light.svg"}
+        position="absolute"
+        zIndex={-1}
+        objectFit="cover" // Assicurati che l'immagine copra l'intera area senza perdere le proporzioni
+        objectPosition="center" // Centra l'immagine nella viewport
+      />
       <Outlet />
     </VStack>
   );
--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,8 +1,8 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Button, HStack, IconButton, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
+import { FaMoon, FaSun } from "react-icons/fa";
 export const TopBar = () => {
-  // const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack w="full" justify="space-between" p={2} >
@@ -13,6 +13,18 @@ export const TopBar = () => {
         <Link to="/demo">
           <Button>Demo</Button>
         </Link>
+        <IconButton
+        aria-label="toggle theme"
+        rounded="full"
+        // size="xs"
+        // position="absolute"
+        // bottom={4}
+        // left={4}
+        onClick={toggleColorMode} icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
+        />
+        {/* <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button> */}
       </HStack>
       {/* <Button variant="text" onClick={toggleColorMode}>
           Toggle {colorMode === "light" ? "Dark" : "Light"}
