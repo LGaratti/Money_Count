1b479a41cecb9d0157480a0494e73055fa32b467
LucaGaratti <l.garatti1996@gmail.com>
2023-12-21 19:03:06 +0100

    create handleAmountChange Modified HStack with Wrap in HomePage

--- src/components/organisms/AddOperationForm.tsx
+++ src/components/organisms/AddOperationForm.tsx
@@ -1,89 +1,71 @@
-import { Card, CardBody, Heading, BoxProps, CardHeader, FormControl, FormLabel, Input, Button, HStack, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper, Spacer, VStack, Box} from '@chakra-ui/react'
-import { useState } from 'react';
+import { 
+  Card, CardBody, Heading, BoxProps, CardHeader, FormControl,
+  FormLabel, Input, Button, HStack, NumberInput, NumberInputField,
+  NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper,
+  Spacer, VStack, Tag, Wrap, WrapItem} from '@chakra-ui/react'
+import {  useState } from 'react';
 // import { useTranslation } from 'react-i18next';
 // import i18n from '../../locales/i18n';
 // import { Operation } from '../../interfaces/Operation';
 
 export const AddOperationBox = ({...props}: BoxProps) => {
   // const {t} = useTranslation('ns1',{ i18n } );
-  const formatAmount = (val: string) => val+' €'; 
-  const parseAmount = (val: string) => val.replace(/^€/, '');
-  const [amount, setAmount] = useState('');
+  // const formatAmount = (val: string) => val+' €'; 
+  // const parseAmount = (val: string) => val.replace(/^€/, '');
 
+  const [_, setAmount] = useState(0);
+  const handleAmountChange = (_: string, valueAsNumber: number) => {
+    setAmount(valueAsNumber);
+  };
 //DA SCOMMENTARE
-//   const [labels, setLabels] = useState([]);
-// 
-//   useEffect(() => { 
-//     const amountValue = parseFloat(amount);
-//     if (amountValue < 0) {
-//       setLabels()
-//     }
-//   }, [amount]);
+  // const [labels, setLabels] = useState([]);
+
+  // useEffect(() => { 
+  //   if (amount < 0) {
+  //     setLabels()
+  //   }
+  // }, [amount]);
 
   // const { colorMode } = useColorMode();
   return (
     <> 
-    <Card  {...props}>
-      <CardHeader>
-        <Heading>Add Operation</Heading>
-      </CardHeader>
-      <CardBody>
-        <FormControl>
-          <VStack>
-            <HStack>
-              <VStack>
-                <FormLabel>name</FormLabel>
-                <Input type="text" placeholder="Inserisci il nome" />
-              </VStack>
-              <VStack>
-                <FormLabel>description</FormLabel>
+      <Card {...props}>
+        <CardHeader>
+          <Heading>Add Operation</Heading>
+        </CardHeader>
+        <CardBody>
+          <FormControl>
+            <VStack>
+              <HStack>
+                <FormLabel>Name</FormLabel>
+                <Input size={'sm'} type="text" placeholder="Inserisci il nome" />
+                <FormLabel>Description</FormLabel>
                 <Input type="text" placeholder="Descrizione" />
-              </VStack>
-              <VStack>
-                <FormLabel>amount</FormLabel>
-                <NumberInput precision={2}
-                  onChange={(valueString) => setAmount(parseAmount(valueString))}
-                  value={formatAmount(amount)}>
+              </HStack>
+              <Spacer/>            
+              <HStack>
+                <FormLabel>Labels</FormLabel>
+                <Wrap>
+                  {[...Array(6)].map((_, index) => (
+                    <WrapItem key={index}>
+                      <Tag>Pippo</Tag>
+                    </WrapItem>
+                  ))}
+                </Wrap>
+                <FormLabel>Amount</FormLabel>
+                <NumberInput precision={2} onChange={handleAmountChange}>
                   <NumberInputField />
                   <NumberInputStepper>
                     <NumberIncrementStepper />
                     <NumberDecrementStepper />
                   </NumberInputStepper>
                 </NumberInput>
-              </VStack>
-            </HStack>
-
-            <Spacer/>
-            
-            <HStack>
-              <VStack>
-                <FormLabel>labels</FormLabel>
-                <Box>
-                  <HStack></HStack>
-                </Box>
-              </VStack>
-              <VStack>
-                <FormLabel>Descrizione</FormLabel>
-                <Input type="text" placeholder="Descrizione" />
-              </VStack>
-              <VStack>
-                <FormLabel>Ammontare</FormLabel>
-                <NumberInput>
-                  <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </VStack>
-            </HStack>
-          </VStack>
-          
-          {/* Qui inserisci il componente DatePicker */}
-          <Button colorScheme="green" mt={4}>Aggiungi</Button>
-        </FormControl>
-      </CardBody>
-    </Card>
+              </HStack>
+            </VStack>
+            <Button colorScheme="green" mt={4}>Aggiungi</Button>
+          </FormControl>
+        </CardBody>
+      </Card>
     </>
   )
 }
--- src/utils/OperationUtils.ts
+++ src/utils/OperationUtils.ts
@@ -1,4 +1,3 @@
-
 // import axios from 'axios';
 
 import { Label, Operation, OperationsAction, TimeUnit } from "../interfaces/Operation";
