6292817e32687d596eba9da7bb9abe0573189498
Luca Garatti <l.garatti1996@gmail.com>
2024-01-03 11:22:52 +0100

    Modified Homepage and OperationsCard

--- src/components/molecules/OperationsCard.tsx
+++ src/components/molecules/OperationsCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Table, TableContainer, CardProps, Tag, Tbody, Td, Th, Thead, Tr, Wrap} from '@chakra-ui/react'
+import { Card, CardBody, Heading, Table, CardProps, Tag, Tbody, Td, Th, Thead, Tr, Wrap, useColorMode} from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next';
 import i18n from '../../locales/i18n';
 import { Operation } from '../../interfaces/Operation';
@@ -10,41 +10,37 @@ interface OperationsCardProps extends CardProps {
 
 export const OperationsCard = ({operations, cardTitle, ...props}: OperationsCardProps) => {
   const {t} = useTranslation('ns1',{ i18n } );
-
+  const { colorMode } = useColorMode();
   return (
-    <> 
-    <Card {...props}>
+    <Card minHeight="100%"{...props}>
       <CardBody>
       <Heading size={'sm'}>{t(cardTitle)}</Heading>
-        <TableContainer>
-          <Table variant='simple'>
-            <Thead>
-              <Tr>
-                <Th>{t('name')}</Th>
-                <Th>{t('amount')}</Th>
-                <Th>{t('labels')}</Th>
+        
+        <Table colorScheme={colorMode === 'dark' ? 'teal' : 'gray'} variant='simple' >
+          <Thead>
+            <Tr>
+              <Th>{t('name')}</Th> <Th>{t('amount')}</Th> <Th>{t('labels')}</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {operations?.map((operation) => (
+              <Tr key = {operation.operation_id}>
+                <Td>{operation?.name}</Td>
+                <Td isNumeric color={ operation?.amount >= 0 ? 'green' : 'red'}> {
+                  ( operation?.amount >= 0 ? '+': '') + operation.amount + ' €'
+                }</Td>
+                <Td>
+                <Wrap spacing={2}>
+                  {operation?.labels.map((label) => (<Tag key={label?.label_id} color={'#1a202c'} bg={label?.color_rgb+'.100'}>{label.name}</Tag>))}
+                </Wrap>
+                </Td>
               </Tr>
-            </Thead>
-            <Tbody>
-              {operations?.map((operation) => (
-                <Tr key = {operation.operation_id}>
-                  <Td>{operation?.name}</Td>
-                  <Td color={ operation?.amount >= 0 ? 'green' : 'red'}> {
-                    ( operation?.amount >= 0 ? '+': '') + operation.amount + ' €'
-                  }</Td>
-                  <Td>
-                  <Wrap spacing={2}>
-                    {operation?.labels.map((label) => (<Tag key={label?.label_id} color={'#1a202c'} bg={label?.color_rgb+'.100'}>{label.name}</Tag>))}
-                  </Wrap>
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
+            ))}
+          </Tbody>
+        </Table>
+        
       </CardBody>
     </Card>
-    </>
   )
 }
 
