70d59a6b99d66625ff084e06f144f6654456f619
Luca Garatti <l.garatti1996@gmail.com>
2024-01-14 11:26:58 +0100

    AddOperation Button Added at topbar

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,9 +1,14 @@
 import { Heading, AbsoluteCenter, useColorMode, Switch, HStack, Spacer, Icon, Button, Wrap } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { FaMoon, FaSun } from "react-icons/fa";
+import { MdPostAdd } from "react-icons/md";
+import { useTranslation } from "react-i18next";
+import i18n from "../../locales/i18n";
 
 export const TopBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const {t} = useTranslation('ns1',{ i18n } );
+
   return (
     <HStack w="full" p={2} justify="space-between">
       <Wrap spacing={4}>
@@ -24,6 +29,16 @@ export const TopBar = () => {
         </AbsoluteCenter>
       </HStack>
       <Spacer />
+      <HStack>
+        <Link to="/demo">
+          <Button
+            rightIcon={<MdPostAdd/>}
+            bg={colorMode === 'light' ? 'primary.600' : 'primary.300'} 
+            color={colorMode === 'light' ? 'white' : 'black'}>
+            {t('add operation')}
+          </Button>
+        </Link>
+      </HStack>
       <HStack>
         <Switch variant={'switchTheme'}
           isChecked={colorMode === 'dark'}
