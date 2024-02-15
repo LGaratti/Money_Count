bd403a473c702f9c43ca19617cc0b5f30f28ffc4
LucaGaratti <l.garatti1996@gmail.com>
2024-02-15 17:38:22 +0100

    Divided form into two forms with relative ref and relative submitHandler

--- src/components/organisms/AddOperationModal.tsx
+++ src/components/organisms/AddOperationModal.tsx
@@ -15,13 +15,13 @@ import { Modal, ModalBody, ModalHeader, FormControl, FormLabel, Input, Button, N
   StepDescription,
   StepSeparator,
   ModalFooter } from '@chakra-ui/react'
-import {  useEffect, useState } from 'react';
+import {  useEffect, useRef, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Label, Operation } from '../../interfaces/Operation';
 import { useTranslation } from 'react-i18next';
 import i18n from '../../locales/i18n';
 import LabelsComponent from '../molecules/LabelsComponent';
-import { InsertOpFromServer, fetchLabelsFromServer } from '../../utils/supabaseClient';
+import { fetchLabelsFromServer } from '../../utils/supabaseClient';
 import { FaEuroSign } from 'react-icons/fa';
 import { TimeUnit } from '../../interfaces/Date';
 
@@ -32,6 +32,12 @@ export const AddOperationModal = ({...props}:ModalProps) => {
   const [amount, setAmount] = useState(0);
   const [periodic_unit, setPeriodic_unit] = useState<TimeUnit>();
 
+  // Labels updated from server at first render
+  useEffect(() => { 
+    fetchLabelsFromServer(setServerLabels);
+  },[]);
+
+  // gain and loss label assigned due to amount sign
   useEffect(() => {
     let tempLabels = [...labels];
     tempLabels = tempLabels.filter(label => label.name !== 'gain' && label.name !== 'loss');
@@ -56,25 +62,6 @@ export const AddOperationModal = ({...props}:ModalProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [amount]);
 
-  useEffect(() => { 
-    fetchLabelsFromServer(setServerLabels);
-  },[]);
-
-  const {
-    handleSubmit,
-    register,
-    // formState: { errors, isSubmitting },
-    formState: { errors },
-  } = useForm<Operation>();
-
-  const onSubmit: SubmitHandler<Operation> = (data) => {
-    data.labels = labels;
-    data.first_date;
-    if(data?.periodic_unit === 'none')
-      data.periodic_unit = undefined;
-    InsertOpFromServer(data);
-  };
-
   const handlePeriodicUnitChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value as TimeUnit;
     setPeriodic_unit(value);
@@ -84,6 +71,54 @@ export const AddOperationModal = ({...props}:ModalProps) => {
     setAmount(valueAsNumber);
   };
 
+  // Form Handling
+  // Stato per mantenere i dati del primo step
+  const [stepOneData, setStepOneData] = useState({});
+  const formStep1Ref = useRef<HTMLFormElement>(null);
+  const formStep2Ref = useRef<HTMLFormElement>(null); 
+
+
+  const {
+    handleSubmit,
+    register,
+    // formState: { errors, isSubmitting },
+    formState: { errors },
+  } = useForm<Operation>();
+
+  // Gestione del submit per il primo form
+  const onSubmitStep1: SubmitHandler<Operation> = (data) => {
+    data.labels = labels;
+    // Salva i dati del primo step nello stato
+    console.log(data)
+    setStepOneData(data);
+    // Passa al prossimo step
+    handleStep(true);
+  };
+
+  // OLD
+  // const onSubmitStep2: SubmitHandler<Operation> = (data) => {
+  //   data.labels = labels;
+  //   data.first_date;
+  //   if(data?.periodic_unit === 'none')
+  //     data.periodic_unit = undefined;
+  //   InsertOpFromServer(data);
+  // };
+  const onSubmitStep2: SubmitHandler<Operation> = (data) => {
+    // Combina i dati del primo e secondo step
+    const finalData = {
+      ...stepOneData,
+      ...data,
+      labels,
+      first_date: data.first_date,
+      periodic_unit: data?.periodic_unit === 'none' ? undefined : data.periodic_unit,
+    };
+  
+    console.log("SPARATOOOOOOOOO", finalData);
+    // Inserimento dei dati combinati al server TODO DA SCOMMENTARE
+    // InsertOpFromServer(finalData);
+  };
+
+  // Step Handling
   const steps = [
     {title:"first step", description: "operation info"},
     {title:"second step", description: "temporal info"}
@@ -94,14 +129,24 @@ export const AddOperationModal = ({...props}:ModalProps) => {
     count: steps.length,
   })
 
-  const handleStep = (increase:boolean) => {
+  // Modifica la funzione handleStep per accettare una funzione callback opzionale
+  const handleStep = (increase: boolean, callback?: () => void) => {
     const nextStep = increase ? activeStep + 1 : activeStep - 1;
     
-    // Verifica se il prossimo step è all'interno dei limiti degli step definiti
-    if (nextStep >= 0 && nextStep <= 1) {
+    if (nextStep >= 0 && nextStep < steps.length) {
       setActiveStep(nextStep);
+      if (callback) callback(); // Esegui la callback se presente
     }
-  }
+  };
+
+  // Quando l'utente clicca su "Next" nel primo step
+  const handleNextClick = () => {
+    if (formStep1Ref.current) {
+      // Questo simula un evento di submit sul form del primo step // TODO cercare cancelable e bubbles
+      formStep1Ref.current.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+    }
+  };
+
 
   return (
     <> 
@@ -132,121 +177,128 @@ export const AddOperationModal = ({...props}:ModalProps) => {
                   </Step>
                 ))}
             </Stepper>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            {/* OLD <form onSubmit={handleSubmit(onSubmitStep2)}> */}
               
               { activeStep == 0 ?
                 (
-                  <Grid templateRows='repeat(2, 1fr)' templateColumns='repeat(3, 1fr)'gap={4}>
-                    <GridItem>
-                      <FormControl isInvalid={!!errors.name}>
-                        <FormLabel htmlFor='name'>{t('name')}</FormLabel>
-                        <Input size={'sm'} id='name' {...register('name', { required: t('required field') })} />
-                        <FormErrorMessage>
-                          {errors.name && errors.name.message}
-                        </FormErrorMessage>
+                  <form key="step1" ref={formStep1Ref} onSubmit={handleSubmit(onSubmitStep1)}>
+                    <Grid templateRows='repeat(2, 1fr)' templateColumns='repeat(3, 1fr)'gap={4}>
+                      <GridItem>
+                        <FormControl isInvalid={!!errors.name}>
+                          <FormLabel htmlFor='name'>{t('name')}</FormLabel>
+                          <Input size={'sm'} id='name' {...register('name', { required: t('required field') })} />
+                          <FormErrorMessage>
+                            {errors.name && errors.name.message}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                        <FormControl isInvalid={!!errors.amount}>
+                          <FormLabel htmlFor='amount'>{t('amount')}</FormLabel>
+                            <NumberInput size={'sm'} id='amount' precision={2} min={-9999.99} max={9999.99} onChange={handleAmountChange}>
+                              <NumberInputField pattern="(-)?[0-9]*(.[0-9]+)?" {...register('amount', {
+                                valueAsNumber: true,
+                                required: t('required field'),
+                                validate: value => value !== 0 || t('amount equal to 0')
+                              })} />
+                              <NumberInputStepper top={'calc(100% - 1.5rem )'}>
+                                <FaEuroSign/>
+                              </NumberInputStepper>
+                            </NumberInput>
+                          <FormErrorMessage>
+                            {errors.amount && errors.amount.message}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                      <FormControl>
+                        <FormLabel htmlFor='labels'>{t('labels')}</FormLabel>
+                        <LabelsComponent serverLabels={serverLabels} setServerLabels={setServerLabels} labels={labels} setLabels={setLabels} {...register('labels')}/>
                       </FormControl>
                     </GridItem>
-                    <GridItem>
-                      <FormControl isInvalid={!!errors.amount}>
-                        <FormLabel htmlFor='amount'>{t('amount')}</FormLabel>
-                          <NumberInput size={'sm'} id='amount' precision={2} min={-9999.99} max={9999.99} onChange={handleAmountChange}>
-                            <NumberInputField pattern="(-)?[0-9]*(.[0-9]+)?" {...register('amount', {
-                              valueAsNumber: true,
-                              required: t('required field'),
-                              validate: value => value !== 0 || t('amount equal to 0')
-                            })} />
-                            <NumberInputStepper top={'calc(100% - 1.5rem )'}>
-                              <FaEuroSign/>
+                      <GridItem colSpan={3}>
+                        <FormControl isInvalid={!!errors.description}>
+                          <FormLabel htmlFor='description'>{t('description')}</FormLabel>
+                          <Textarea id='description' {...register('description')}></Textarea>
+                          <FormErrorMessage>
+                            {errors.description && errors.description.message}
+                          </FormErrorMessage>
+                      </FormControl>
+
+                      </GridItem>
+                    </Grid>
+                  </form>
+                ) : (
+                  <form key="step2" ref={formStep2Ref} onSubmit={handleSubmit(onSubmitStep2)}>
+                    <Grid templateRows='repeat(2, 1fr)' templateColumns='repeat(2, 1fr)'gap={4}>
+                      <GridItem>
+                        <FormControl isInvalid={!!errors.first_date}>
+                          <FormLabel htmlFor='first_date'>{t('date')}</FormLabel>
+                          <Input
+                            id='first_date'
+                            placeholder="Select Date and Time"
+                            size={'sm'}
+                            type="date"
+                            {...register('first_date', { required: t('required field') })}
+                          />
+                          <FormErrorMessage>
+                            {errors.first_date && errors.first_date.message}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                        <FormControl isInvalid={!!errors.periodic_unit}>
+                          <FormLabel htmlFor='periodic_unit'>{t('recurrence')}</FormLabel>
+                          <Select size={'sm'} id='periodic_unit' {...register('periodic_unit')} onChange={handlePeriodicUnitChange} >
+                            <option value='none'>{t('none')}</option>
+                            <option value='day'>{t('dayly')}</option>
+                            <option value='week'>{t('weekly')}</option>
+                            <option value='month'>{t('monthly')}</option>
+                            <option value='year'>{t('yearly')}</option>
+                          </Select>
+                          <FormErrorMessage>
+                            {errors.periodic_unit && errors.periodic_unit.message}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem>
+                        {periodic_unit && periodic_unit !== 'none' && (
+                        <FormControl isInvalid={!!errors.periodic_count}>
+                          <FormLabel htmlFor='periodic_count'>{t('recurrence count')}</FormLabel>
+                          <NumberInput defaultValue={0} id='periodic_count' size={'sm'}>
+                            <NumberInputField {...register('periodic_count', { valueAsNumber: true, required: t('required field') })} />
+                            <NumberInputStepper>
+                              <NumberIncrementStepper />
+                              <NumberDecrementStepper />
                             </NumberInputStepper>
                           </NumberInput>
-                        <FormErrorMessage>
-                          {errors.amount && errors.amount.message}
-                        </FormErrorMessage>
-                      </FormControl>
-                    </GridItem>
-                    <GridItem>
-                    <FormControl>
-                      <FormLabel htmlFor='labels'>{t('labels')}</FormLabel>
-                      <LabelsComponent serverLabels={serverLabels} setServerLabels={setServerLabels} labels={labels} setLabels={setLabels} {...register('labels')}/>
-                    </FormControl>
-                  </GridItem>
-                    <GridItem colSpan={3}>
-                      <FormControl isInvalid={!!errors.description}>
-                        <FormLabel htmlFor='description'>{t('description')}</FormLabel>
-                        <Textarea id='description' {...register('description')}></Textarea>
-                        <FormErrorMessage>
-                          {errors.description && errors.description.message}
-                        </FormErrorMessage>
-                    </FormControl>
-
-                    </GridItem>
-                  </Grid>
-                ) : (
-                  <Grid templateRows='repeat(2, 1fr)' templateColumns='repeat(2, 1fr)'gap={4}>
-                    <GridItem>
-                      <FormControl isInvalid={!!errors.first_date}>
-                        <FormLabel htmlFor='first_date'>{t('date')}</FormLabel>
-                        <Input
-                          id='first_date'
-                          placeholder="Select Date and Time"
-                          size={'sm'}
-                          type="date"
-                          {...register('first_date', { required: t('required field') })}
-                        />
-                        <FormErrorMessage>
-                          {errors.first_date && errors.first_date.message}
-                        </FormErrorMessage>
-                      </FormControl>
-                    </GridItem>
-                    <GridItem>
-                      <FormControl isInvalid={!!errors.periodic_unit}>
-                        <FormLabel htmlFor='periodic_unit'>{t('recurrence')}</FormLabel>
-                        <Select size={'sm'} id='periodic_unit' {...register('periodic_unit')} onChange={handlePeriodicUnitChange} >
-                          <option value='none'>{t('none')}</option>
-                          <option value='day'>{t('dayly')}</option>
-                          <option value='week'>{t('weekly')}</option>
-                          <option value='month'>{t('monthly')}</option>
-                          <option value='year'>{t('yearly')}</option>
-                        </Select>
-                        <FormErrorMessage>
-                          {errors.periodic_unit && errors.periodic_unit.message}
-                        </FormErrorMessage>
-                      </FormControl>
-                    </GridItem>
-                    <GridItem>
-                      {periodic_unit && periodic_unit !== 'none' && (
-                      <FormControl isInvalid={!!errors.periodic_count}>
-                        <FormLabel htmlFor='periodic_count'>{t('recurrence count')}</FormLabel>
-                        <NumberInput defaultValue={0} id='periodic_count' size={'sm'}>
-                          <NumberInputField {...register('periodic_count', { valueAsNumber: true, required: t('required field') })} />
-                          <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                          </NumberInputStepper>
-                        </NumberInput>
-                        <FormErrorMessage>
-                          {errors.periodic_count && errors.periodic_count.message}
-                        </FormErrorMessage>
-                      </FormControl>
-                      )}
-                    </GridItem>
-                  </Grid>
+                          <FormErrorMessage>
+                            {errors.periodic_count && errors.periodic_count.message}
+                          </FormErrorMessage>
+                        </FormControl>
+                        )}
+                      </GridItem>
+                    </Grid>
+                  </form>
                 )
               }              
-            </form>
           </ModalBody>
           <ModalFooter display={'flex'} justifyContent="space-between">
-          {/* <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'> Add </Button> */}
           {
             activeStep < 1 ? (
               <>
                 <Box></Box>
-                <Button onClick={() => handleStep(true)}>{t('next')}</Button>
+                <Button onClick={handleNextClick}>{t('next')}</Button>
               </>
             ) : (
               <>
                 <Button onClick={() => handleStep(false)}>{t('previous')}</Button>
-                <Button colorScheme='green' type='submit'>{t('add')}</Button>
+                <Button colorScheme='green' onClick={() => {
+                  if (formStep2Ref && formStep2Ref.current) {
+                    // Questo simula un evento di submit sul form del secondo step
+                    formStep2Ref.current.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
+                  }
+                }}>{t('add')}</Button>
               </>
             )
           }
